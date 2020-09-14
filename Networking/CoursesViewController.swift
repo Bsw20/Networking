f3216b6c02b3b72607efeb17a059d0fbc5145182
Bsw20 <55621971+KarpunkinYaroslav@users.noreply.github.com>
2020-09-14 17:37:08 +0300

    Setup segue and webView

--- Networking/CoursesViewController.swift
+++ Networking/CoursesViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class CoursesViewController: UIViewController {
     private var courses: [Course] = [Course]()
+    private var courseName: String?
+    private var courseUrl: String?
+    
     @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -27,14 +30,54 @@ class CoursesViewController: UIViewController {
             guard let data = data else  { return }
             
             do {
-                self.courses  = try JSONDecoder().decode([Course].self, from: data)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                self.courses  = try decoder.decode([Course].self, from: data)
                 
                 print("\(self.courses)")
             } catch let error {
                 print("\(error) error serialization json")
             }
             
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+            
         }).resume()
+
+    }
+    private func configureCell(cell: TableViewCell, for indexPath: IndexPath){
+        let course = courses[indexPath.row]
+        cell.courseNameLabel.text = course.name
+        
+        if let numberOfLessons = course.numberOfLessons {
+            cell.numberOfLessons.text = "Number of lessons: \(numberOfLessons)"
+        }
+        
+        if let numberOfTests = course.numberOfTests{
+            cell.numberOfTests.text = "Number of tests: \(numberOfTests)"
+        }
+        
+        DispatchQueue.global().async {
+            guard let stringUrl = course.imageUrl else  { return }
+            guard let imageUrl = URL(string: stringUrl) else  { return }
+            guard let imageData = try? Data(contentsOf: imageUrl) else { return }
+            
+            DispatchQueue.main.async {
+                cell.courseImage.image = UIImage(data: imageData)
+            }
+        }
+
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let webViewController = segue.destination as? WebViewController else  { return }
+        
+        webViewController.selectedCourse = courseName
+        if let url = courseUrl {
+            webViewController.courseURL = url
+        }
+        
     }
 
 }
@@ -50,6 +93,7 @@ extension CoursesViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! TableViewCell
+        configureCell(cell: cell, for: indexPath)
         
         return cell
     }
@@ -61,6 +105,15 @@ extension CoursesViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let course = courses[indexPath.row]
+        
+        courseUrl = course.link
+        courseName = course.name
+        
+        performSegue(withIdentifier: "Description", sender: self)
     }
 }
 
